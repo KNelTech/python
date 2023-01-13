10aa5449748494efbf5928b50fd29f4e8a31059f
knel tech <k.nelsontech@gmail.com>
2023-01-12 18:39:05 -0600

    RPS beginning

---
+++ RockPaperScissors/RPS.py
@@ -0,0 +1,8 @@
+import random
+
+userActions = input("enter a choice: Rock, Paper or Scissors\n")
+
+#The brackets are a mutable list and the parentheses are immutable tuple
+possibleActions = ["Rock", "Paper", "Scissors"]
+
+Computer
